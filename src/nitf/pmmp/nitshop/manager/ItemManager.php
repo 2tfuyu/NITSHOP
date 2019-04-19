7e8eaf556a7fb1ba1a718b9e5e351ce8f07eb56f
github nitf <2tf2003@gmail.com>
2019-04-19 22:52:44 +0900

    [add] Config関連

--- src/nitf/pmmp/nitshop/manager/ItemManager.php
+++ src/nitf/pmmp/nitshop/manager/ItemManager.php
@@ -3,10 +3,9 @@
 namespace nitf\pmmp\nitshop\manager;
 
 use nitf\pmmp\nitshop\config\ConfigManager;
+use pocketmine\item\Item;
 
 class ItemManager extends ConfigManager {
-
-    private $items = [];
 
     public function __construct(){
 
@@ -14,5 +13,22 @@ class ItemManager extends ConfigManager {
 
     public function getItems(bool $key = false): array {
         return $this->getConfig()->getAll($key);
+    }
+
+    public function getData(string $name): ?array {
+        return $this->getConfig()->get($name);
+    }
+
+    public function addItem(Item $item, string $name, int $price = 0, int $amount = 0): void {
+        $data = [
+            "id" => $item->getId(),
+            "price" => $price,
+            "amount" => $amount
+        ];
+        $this->getConfig()->set($name, $data);
+    }
+
+    public function removeItem(string $name): void {
+        $this->getConfig()->remove($name);
     }
 }

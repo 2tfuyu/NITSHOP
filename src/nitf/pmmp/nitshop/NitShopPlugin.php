e716a3595cbb385dbdc8198c019bb3efcc5f47f7
github nitf <2tf2003@gmail.com>
2019-04-22 20:04:06 +0900

    [add] コマンド登録＆フォルダ追加処理

--- src/nitf/pmmp/nitshop/NitShopPlugin.php
+++ src/nitf/pmmp/nitshop/NitShopPlugin.php
@@ -2,10 +2,19 @@
 
 namespace nitf\pmmp\nitshop;
 
+use nitf\pmmp\nitshop\command\ShopCommand;
+use nitf\pmmp\nitshop\config\ConfigManager;
 use pocketmine\plugin\PluginBase;
 
 class NitShopPlugin extends PluginBase {
 
     public function onEnable(): void {
+        $path = $this->getDataFolder();
+        if (!file_exists($path)) {
+            @mkdir($path);
+        }
+        $this->getServer()->getCommandMap()->register('shop', new ShopCommand());
+
+        ConfigManager::init($path, ["Item", "Form"]);
     }
 }

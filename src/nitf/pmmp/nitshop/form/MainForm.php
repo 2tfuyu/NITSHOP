d7103e8497d9c2f18f02139c422b9cc865a702db
github nitf <2tf2003@gmail.com>
2019-04-22 20:03:08 +0900

    [add] 処理の追記

--- src/nitf/pmmp/nitshop/form/MainForm.php
+++ src/nitf/pmmp/nitshop/form/MainForm.php
@@ -5,18 +5,29 @@ namespace nitf\pmmp\nitshop\form;
 use pocketmine\Player;
 use jojoe77777\FormAPI\SimpleForm;
 
-class MainForm extends Form {
+class MainForm extends Form implements ButtonIds {
+
+    public function __construct() {
+    }
 
     public function register(): void {
         $function = function (Player $player, int $button = 0) {
-
+            if (empty($button)) {
+                return;
+            }
+            switch ($button) {
+                case ButtonIds::BACK_BUTTON:
+                    break;
+                case ButtonIds::BLOCK_BUTTON:
+                    break;
+            }
         };
         $data = [
             "title" => "MainForm",
             "content" => "",
             "button" => [
-                "sample",
-                "sample"
+                "close",
+                "block"
             ]
         ];
         parent::__construct($function, $data, SimpleForm::class);
